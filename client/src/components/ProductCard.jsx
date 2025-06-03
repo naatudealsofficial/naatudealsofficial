21a03696f0fa473376ff9c882a903c4bbc07dadf
rakeshboyiri <boyirirakesh@gmail.com>
2025-06-03 11:20:01 +0530

    fixed the bugs

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,21 +1,21 @@
 function ProductCard({ product }) {
-    return (
-      <div className="bg-white rounded-xl shadow-md p-4">
-        <a
-          href={product.affiliateLink}
-          className="text-blue-500 text-sm mt-2 block"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <img src={product.imageUrl} alt={product.name} className="w-full h-40 object-cover rounded" />
-        <h2 className="text-xl font-bold mt-2">{product.name}</h2>
-        {/* <p className="text-sm">{product.description}</p> */}
-        {/* <p className="font-semibold text-green-600 mt-1">${product.price}</p> */}
-        
-        </a>
-      </div>
-    );
-  }
-  
-  export default ProductCard;
-  
+  return (
+    <div className="border border-gray-300 rounded-md overflow-hidden w-full text-center shadow-md bg-white">
+      <a
+        href={product.affiliateLink}
+        className="block p-4"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={product.imageUrl}
+          alt={product.name}
+          className="w-full h-40 object-contain mx-auto"
+        />
+        <h2 className="text-md font-semibold mt-2">{product.name}</h2>
+      </a>
+    </div>
+  );
+}
+
+export default ProductCard;
